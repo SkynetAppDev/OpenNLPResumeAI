c4f934184d25b450d55634c409ba262a3bdceceb
JayKesavan <JayKesavan@192.168.4.1>
2017-08-25 20:53:51 -0400

    Test

---
+++ OpenNLPResumeAI/src/openNLPResumeAI/OpenNLPSentenceDetection.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package openNLPResumeAI;
+
+/**
+ * @author JayKesavan
+ *
+ */
+public class OpenNLPSentenceDetection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
